10797148ef4dfa3dd842fd8bc651b3a7cab0e34a
readywater <andrew@relaystudio.com>
2016-01-28 13:08:30 -0800

    )@#%U@*%#

--- func.py
+++ func.py
@@ -1,24 +1,32 @@
 import re
 import pandas as pd
 
-def parse_train(x):
-    # x should be the train code eg 123
+def parse_train(l):
+    # x should be a list with train codes eg 123
     # {"id": "123", "type:" "bullet", direction: "south"}
-    x = str(x)
-    if x == '': return ''
-    elif len(x) != 3: return ''
-    t = {}
-    t["id"] = x if len(x) is 3 else -1
-    t["direction"] = "north" if x[2] in [1,3,5,7,9] else "south"
-    if x[0] == '1':
-        t["type"] = "local"
-    elif x[0] == '2':
-        t["type"] = "limited"
-    elif x[0] == '3':
-        t["type"] = "bullet"
-    else:
-        t["type"] = "Unknown"
-    ret = pd.Series({'train_id':t["id"], 'train_direction':t["direction"], 'train_type': t["type"] })
+    # Iterate through list
+    s = l['topic_train']
+    ret = []
+    for x in s:
+        t = {}
+        # Check train id
+        x = str(x)
+        if x == '': return ''
+        elif len(x) != 3: return ''
+        t["id"] = x if len(x) is 3 else -1
+
+        # 1 = north, 2 = south
+        t["direction"] = 1 if x[2] in [1,3,5,7,9] else 0
+
+        if x[0] == '1':
+            t["type"] = 0 # local
+        elif x[0] == '2':
+            t["type"] = 1 # limited
+        elif x[0] == '3':
+            t["type"] = 2 # bullet
+        else:
+            t["type"] = -1
+        ret.append(pd.Series({'tweet_id': l.id, 'train_id':t["id"], 'train_direction':t["direction"], 'train_type': t["type"] }))
     return ret
     
 def get_time_of_day(x):
@@ -43,8 +51,26 @@ def get_time_of_day(x):
 def check_hashtag(x):
     # x should be the cleaned hashtag row
     if x == '': return ''
-    t = re.search('[N,S]B([0-9]{0,3})',x)
+    t = re.findall('[N,S]B([0-9]{0,3})',x)
     if t:
-        return t.group(1)
+        return t
     else:
         return ''
+    
+def check_late(x):
+        # x should be the cleaned hashtag row
+    if x == '': return ''
+    t = re.findall('[N,S]B([0-9]{0,3})',x)
+    if t:
+        return t
+    else:
+        return ''
+    
+def rank_late(x):
+    # consumes pd.series
+    # Consumes a series. Takes parsed LATE and TRAIN lists, and spits out a magnitude of delay value
+    # 0 = No delay, 4 = BIG delay
+    
+    x = pd.Series(x) # Cast just in case
+    trains = x.train_topic
+    
